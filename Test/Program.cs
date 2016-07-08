f9e44ba0aa3b9ab068b1452d6323c8c1e2676c89
Petro Vozhdai <petro.vozhdai@D2L.com>
2016-07-08 13:16:18 +0300

    SOLID staff

--- Test/Program.cs
+++ Test/Program.cs
@@ -2,6 +2,7 @@
 using Patterns.Structural;
 using Patterns.Creational;
 using Patterns.Behavioural;
+using Patterns.SOLID._1SingleResponsibilityPrinciple;
 
 namespace Patterns
 {
@@ -9,22 +10,33 @@ namespace Patterns
     {
         static void Main(string[] args)
         {
-            //Car car = new Car(new ElectricCar());
-            //car.Move();
 
-            //car.Vehicle = new PetrolCar();
-            //car.Move();
+			ProductB p1 = new ProductB {Price = 100};
+	        Console.WriteLine( p1.IsValid( new ValidatorA() ) );
+			p1.Price = 10;
+	        Console.WriteLine( p1.IsValid( new ValidatorB() ) );
+			p1.Price = 0;
+			Console.WriteLine( p1.IsValid( new ValidatorA() ) );
 
 
-            Pizza p = new ItalianPizza();
-            Console.WriteLine(p.GetCost());
 
-            p = new CheesePizza(p);
-            Console.WriteLine(p.GetCost());
 
-            p = new TomatoPizza(p);
-            Console.WriteLine(p.GetCost());
-            Console.ReadKey();
+			//Car car = new Car(new ElectricCar());
+			//car.Move();
+
+			//car.Vehicle = new PetrolCar();
+			//car.Move();
+
+
+			//Pizza p = new ItalianPizza();
+			//Console.WriteLine(p.GetCost());
+
+			//p = new CheesePizza(p);
+			//Console.WriteLine(p.GetCost());
+
+			//p = new TomatoPizza(p);
+			//Console.WriteLine(p.GetCost());
+			Console.ReadKey();
         }
     }
 }
